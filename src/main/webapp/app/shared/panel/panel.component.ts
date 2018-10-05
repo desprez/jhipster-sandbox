0025e2b6f011d69c5d8a24ebfd80a34c612d334c
PASCAL Desprez <pascal.desprez@fr.ibm.com>
2018-10-05 23:53:03 +0200

    add new panel component on home page

--- src/main/webapp/app/shared/panel/panel.component.ts
+++ src/main/webapp/app/shared/panel/panel.component.ts
@@ -2,17 +2,14 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
     selector: 'jhi-panel',
-  templateUrl: './panel.component.html',
+    templateUrl: './panel.component.html'
 })
 export class PanelComponent implements OnInit, OnDestroy {
     alerts: any[];
 
-    constructor() { }
+    constructor() {}
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 
-    ngOnDestroy() {
-    }
-
+    ngOnDestroy() {}
 }
--- src/main/webapp/app/shared/shared-common.module.ts
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-
+import { PanelComponent } from 'app/shared/panel/panel.component';
 import { JhipsterSandBoxSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent } from './';
 
 @NgModule({
     imports: [JhipsterSandBoxSharedLibsModule],
-    declarations: [FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent],
-    exports: [JhipsterSandBoxSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent]
+    declarations: [FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent, PanelComponent],
+    exports: [JhipsterSandBoxSharedLibsModule, FindLanguageFromKeyPipe, JhiAlertComponent, JhiAlertErrorComponent, PanelComponent]
 })
 export class JhipsterSandBoxSharedCommonModule {}
